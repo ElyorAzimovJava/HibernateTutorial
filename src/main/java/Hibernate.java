f9db83302d14ab1aa7dff6131a89f0777f6b1594
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-11-08 09:30:32 +0500

    Fetching data from DB  using Hibernate

--- src/main/java/Hibernate.java
+++ src/main/java/Hibernate.java
@@ -5,23 +5,29 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Hibernate {
     public static void main(String[] args) {
-        User user = new User();
-        user.setUsername("User1");
-        user.setFirstName("John");
-        user.setLastName("Doe");
-        user.setBirthDate(LocalDate.of(1980, 1, 1));
+/*        User user = new User();
+        user.setUsername("User2");
+        user.setFirstName("John2");
+        user.setLastName("Doe2");
+        user.setBirthDate(LocalDate.of(1980, 1, 1));*/
         Configuration configuration = new org.hibernate.cfg.Configuration();
         configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         SessionFactory factory = configuration.buildSessionFactory();
         Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(user);
-        transaction.commit();
+       // Transaction transaction = session.beginTransaction();
+       // session.persist(user);
+
+        User user = session.find(User.class, UUID.fromString("93463d02-381e-44be-b48c-faa9af351420"));
+        System.out.println(user);
+/*        transaction.commit();*/
+        session.close();
+        factory.close();
 
 
     }
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -71,4 +71,14 @@ public class User {
     public int hashCode() {
         return Objects.hash(firstName, lastName, username, birthDate);
     }
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", username='" + username + '\'' +
+                    ", birthDate=" + birthDate +
+                    '}';
+        }
 }
